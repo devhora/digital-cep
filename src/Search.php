bd1dd57e90875987f4c5f6686896bdd3d94cb66c
Heitor da Hora <heitordahoracf@gmail.com>
2020-07-23 21:04:38 -0300

    learning PSRs and CodeSniffer

--- src/Search.php
+++ src/Search.php
@@ -2,18 +2,24 @@
 
 namespace Devhora\DigitalCep;
 
-class Search{
+use Devhora\DigitalCep\ws\ViaCep;
+
+class Search
+{
 
     private $url = "https://viacep.com.br/ws/";
 
-    public function getAddressFromZipCode(string $zipCode): array{
-        $zipCode = preg_replace('/[^0-9]im/','',$zipCode);
+    public function getAddressFromZipCode(string $zipCode): array
+    {
+        $zipCode = preg_replace('/[^0-9]im/', '', $zipCode);
 
-        $get = file_get_contents($this->url . $zipCode . "/json");
-
-        //print_r($get);
-
-        return (array) json_decode($get);
+        return $this->getFromServer($zipCode);
     }
 
+    private function getFromServer(string $zipCode): array
+    {
+        $get = new ViaCep();
+
+        return $get->get($zipCode);
+    }
 }
